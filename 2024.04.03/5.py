03947c5d6f8367cecfc8fc3575665a83cd138b72
Ruslan <ruslan-lebedev-1996@mail.ru>
2024-04-07 14:32:04 +0400

    Задание 5

---
+++ 2024.04.03/5.py
@@ -0,0 +1,23 @@
+cell1 = input()
+cell2 = input()
+
+if (cell1[0] == cell2[0]) or (cell1[1] == cell2[1]):
+    print('да')
+else:
+    print('нет')
+    
+#a1
+#b1
+#нет
+
+#a1
+#g7
+#нет
+
+#d2
+#a2
+#да
+
+#d2
+#a2
+#да
