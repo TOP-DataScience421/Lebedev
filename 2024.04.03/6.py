713d3f3a36d5a3d293d7d6be144029af4cdaaac4
Ruslan <ruslan-lebedev-1996@mail.ru>
2024-04-07 14:54:37 +0400

    Задание 6

---
+++ 2024.04.03/6.py
@@ -0,0 +1,23 @@
+cell1 = input()
+cell2 = input()
+
+if -1 <= ord(cell1[0]) - ord(cell2[0]) <= 1 and -1 <= int(cell1[1]) - int(cell2[1]):
+    print('да')
+else:
+    print('нет')
+    
+a1
+b2
+да
+
+a1
+d1
+нет
+
+e4
+d5
+да
+
+g2
+d2
+нет
