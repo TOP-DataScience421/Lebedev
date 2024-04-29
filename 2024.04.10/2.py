7068687e2b49e640a28c3ada0a625d8affda716b
Ruslan <ruslan-lebedev-1996@mail.ru>
2024-04-29 16:46:27 +0400

    Задание 2

---
+++ 2024.04.10/2.py
@@ -0,0 +1,18 @@
+total = 0
+
+user_count = int(input())
+
+for n in range(user_count):
+    user_num = int(input())
+    if user_num > 0:
+        total += user_num
+        
+print(total)
+
+#14
+#-5
+#-5
+#10
+#33
+#57
+ 
